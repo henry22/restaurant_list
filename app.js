7d743b99aa405052d56b2822fc37a2a552d3ff13
henry22 <henry811010@gmail.com>
2019-11-03 22:41:47 +0800

    set express-handlebars as the view engine

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const expressHandlebars = require('express-handlebars')
+
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-  res.send('My first express app')
+  res.render('index')
 })
 
 app.listen(port, () => console.log(`The server is listening on port:${port}`))
