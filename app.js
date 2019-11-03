496595e4eb4be3ef9da81ba689bb95a7c0d51c87
henry22 <henry811010@gmail.com>
2019-11-03 23:04:23 +0800

    import restaurant lists from json file and run the each loop to render the lists

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const expressHandlebars = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -9,7 +10,9 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  const restaurants = restaurantList.results
+
+  res.render('index', { restaurants: restaurants })
 })
 
 app.listen(port, () => console.log(`The server is listening on port:${port}`))
